11b198d25a957c78b2bc872617c4987abf684ad6
juanolaechea <42986554+juanolaechea@users.noreply.github.com>
2020-10-15 23:25:16 -0300

    athu

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -1,7 +1,15 @@
+import React, { useState } from "react";
 import styled from "@emotion/styled";
 import { css } from "@emotion/core";
+import Router from "next/router";
 import Layout from "../components/layout/Layout";
-import { Formulario, Campo, InputSubmit, Error } from "../components/ui/Formulario";
+import {
+  Formulario,
+  Campo,
+  InputSubmit,
+  Error,
+} from "../components/ui/Formulario";
+import firebase from "../firebase";
 
 //validaciones
 import useValidacion from "../hooks/useValidacion";
@@ -19,6 +27,7 @@ const Encabesazo = styled.h1`
 `;
 
 export default function CrearCuenta() {
+  const [error, guardarError] = useState(false);
 
   const {
     valores,
@@ -29,10 +38,15 @@ export default function CrearCuenta() {
   } = useValidacion(STATE_INICIAL, validarCrearCuenta, crearCuenta);
 
   const { nombre, email, password } = valores;
-  
 
-  function crearCuenta() {
-    console.log("Creando cuenta ");
+  async function crearCuenta() {
+    try {
+      await firebase.registrar(nombre, email, password);
+      Router.push("/");
+    } catch (error) {
+      console.error("Hubo un error al cargar el usuario", error.message);
+      guardarError(error.message);
+    }
   }
 
   return (
@@ -82,6 +96,8 @@ export default function CrearCuenta() {
             </Campo>
 
             {errores.password && <Error>{errores.password}</Error>}
+            {error && <Error> {error} </Error>}
+
             <InputSubmit type="submit" value="Crear cuenta" />
           </Formulario>
         </>
